0b641fea0922be3b1be4d514bc9c82fd1e101c0e
codingwithamit <64533560+codingwithamit@users.noreply.github.com>
2020-09-29 02:29:22 +0530

    hibernate composite key example
    hibernate composite key example

---
+++ hibernate-composite-key-example/src/main/java/com/codingwithamit/Manager.java
@@ -0,0 +1,32 @@
+package com.codingwithamit;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Manager {
+
+	public static void main(String[] args) {
+
+		CompositeKey compositeKey = new CompositeKey();
+		compositeKey.setId(1);
+		compositeKey.setEmailId("autodidactprogrammer4u@gmail.com");
+		Employee e1 = new Employee();
+		e1.setCompositeKey(compositeKey);
+		e1.setName("Amit");
+
+		Configuration config = new Configuration();
+		config.configure();
+
+		SessionFactory factory = config.buildSessionFactory();
+		Session session = factory.openSession();
+		session.beginTransaction();
+
+		session.save(e1);
+		session.getTransaction().commit();
+		session.close();
+		System.out.println("successfully saved");
+		session.close();
+
+	}
+}
