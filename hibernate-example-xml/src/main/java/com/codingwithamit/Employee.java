0bcf2a68ee598b72140079c9cdfb7ee57740d458
codingwithamit <64533560+codingwithamit@users.noreply.github.com>
2020-09-29 01:32:05 +0530

    hibernate example

---
+++ hibernate-example-xml/src/main/java/com/codingwithamit/Employee.java
@@ -0,0 +1,27 @@
+package com.codingwithamit;
+
+public class Employee {
+	private int id;    
+	private String name;    
+	private String emailId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}   
+	
+	
+}
