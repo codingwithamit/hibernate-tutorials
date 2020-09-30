abab6e375f8a8f4666046c317a0b0b4083c5db5a
codingwithamit <64533560+codingwithamit@users.noreply.github.com>
2020-09-30 17:12:00 +0530

    one to many mapping example

---
+++ hibernate-one-to-many-mapping/src/main/java/com/codingwithamit/Employee.java
@@ -0,0 +1,63 @@
+package com.codingwithamit;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mydb.employee")
+public class Employee {
+	@Id
+	@Column(name = "empId")
+	private int empId;
+	@Column(name = "emailId")
+	private String emailId;
+	@Column(name = "firstName")
+	private String firstName;
+	@Column(name = "lastName")
+	private String lastName;
+	
+	/*
+	 * @ManyToOne private Project Project;
+	 */
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/*
+	 * public void setProject(Project project) { Project = project; }
+	 * 
+	 * public Project getProject() { return Project; }
+	 */
+
+}
